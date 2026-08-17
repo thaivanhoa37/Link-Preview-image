e3169ec143563c24430b15152d9c6fd2c374442b
= <hoatv.21it@vku.udn.vn>
2026-08-17 19:45:16 +0700

    ok

--- api/preview.js
+++ api/preview.js
@@ -77,6 +77,8 @@ function buildBotHtml({ title, description, imageUrl, shortUrl }) {
   <meta property="og:title"              content="${safeTitle}" />
   <meta property="og:description"        content="${safeDesc}" />
   <meta property="og:image"              content="${safeImage}" />
+  <meta property="og:image:width"        content="1200" />
+  <meta property="og:image:height"       content="630" />
   <meta property="og:image:type"         content="image/jpeg" />
   <meta property="og:locale"             content="vi_VN" />
   <meta property="og:site_name"          content="Link Preview" />
